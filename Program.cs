dc4850e827f97faad6f6b104856fb8ca3b1a6ef8
Soomin Song <song.soomin.418@outlook.com>
2025-05-07 10:57:38 -0700

    5.3.1 Complete

--- Program.cs
+++ Program.cs
@@ -8,12 +8,38 @@ while(selection!='q')
     switch (selection)
     {
         case '1':
-            Console.WriteLine("1. You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.\r\n\r\nGiven an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.\r\n\r\nExample 1:\r\n\r\nInput: flowerbed = [1,0,0,0,1], n = 1\r\n\r\nOutput: true\r\n\r\nExample 2:\r\n\r\nInput: flowerbed = [1,0,0,0,1], n = 2\r\n\r\nOutput: false\r\n\r\n");
+            Console.WriteLine("1. You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.\n\nGiven an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.\n\nExample 1:\n\nInput: flowerbed = [1,0,0,0,1], n = 1\n\nOutput: true\n\nExample 2:\n\nInput: flowerbed = [1,0,0,0,1], n = 2\n\nOutput: false\n\n");
+
+            flowerbedDemo([1, 0, 0, 0, 1], 2);
+            flowerbedDemo([1, 0, 0, 0, 1], 2);
             break;
         case '2':
-            Console.WriteLine("2. You are climbing a staircase. It takes n steps to reach the top.\r\n\r\nEach time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?\r\nExample 1:\r\n\r\nInput: n = 2\r\n\r\nOutput: 2\r\n\r\nExplanation: There are two ways to climb to the top.\r\n\r\n1. 1 step + 1 step\r\n\r\n2. 2 steps\r\n\r\n\r\nExample 2:\r\n\r\nInput: n = 3\r\n\r\nOutput: 3\r\n\r\nExplanation: There are three ways to climb to the top.\r\n\r\n1. 1 step + 1 step + 1 step\r\n\r\n2. 1 step + 2 steps\r\n\r\n3. 2 steps + 1 step");
+            Console.WriteLine("2. You are climbing a staircase. It takes n steps to reach the top.\n\nEach time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?\nExample 1:\n\nInput: n = 2\n\nOutput: 2\n\nExplanation: There are two ways to climb to the top.\n\n1. 1 step + 1 step\n\n2. 2 steps\n\n\nExample 2:\n\nInput: n = 3\n\nOutput: 3\n\nExplanation: There are three ways to climb to the top.\n\n1. 1 step + 1 step + 1 step\n\n2. 1 step + 2 steps\n\n3. 2 steps + 1 step\n\n");
             break;
-
-
     }
+}
+
+void flowerbedDemo(int[] flowerbed, int n)
+{
+    Console.WriteLine($"flowerbed = [{string.Join(", ", flowerbed)}]");
+    Console.WriteLine($"n = {n}");
+    Console.WriteLine($"Output: {flowerbedFunction(flowerbed,n)}\n");
+}
+
+bool flowerbedFunction(int[] flowerbed, int n)
+{
+    int numSpots = 0;
+    for(int i = 2;i<flowerbed.Length;i++)
+    {
+        if (flowerbed[i - 2] + flowerbed[i - 1] + flowerbed[i]==0)
+        {
+            flowerbed[i - 1] = 2;
+            numSpots++;
+        }
+    }
+    for(int i = 0; i < flowerbed.Length; i++)
+    {
+        if (flowerbed[i]==2) { flowerbed[i] = 0; }
+    }
+    return numSpots>=n;
 }
